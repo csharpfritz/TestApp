ecbb0ed404e9d81de606abe5a0ab5a5c629e49a2
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-05-17 06:31:30 -0400

    First commit

---
+++ TestApp.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
